78d88f2fc525ad3430c7b37920014eb899070757
Alex Waddell <alexander.waddell1997@gmail.com>
2026-06-26 10:43:28 +0100

    fix: revert redirect route

--- next.config.ts
+++ next.config.ts
@@ -2,16 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  async redirects() {
-    return [
-      {
-        source: "/:path*",
-        has: [{ type: "host", value: "www.subcreation.app" }],
-        destination: "https://subcreation.app/:path*",
-        permanent: true,
-      },
-    ];
-  },
   images: {
     localPatterns: [
       {
